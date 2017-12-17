2348517cd39cfcc6aa3e542fcd09549bd183c262
Damian Pawelec <damianpawelec222@gmial.com>
2017-12-17 14:55:29 +0100

    operacje

---
+++ JavaScript/JS/9-operators.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var liczba1 =13, liczba2 =5; 
+
+var wynik; 
+
+wynik = liczba1 % liczba2; 
+console.log(wynik);
+
+liczba1 -= liczba2;
+console.log(liczba1);
+
+if ("2" === 2) {
+    console.log("prawda");
+}
+else {
+    console.log("falsz");
+}
