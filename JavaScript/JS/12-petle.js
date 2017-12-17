73058b11856151856a8238ae48b75f4620ed0bd2
Damian Pawelec <damianpawelec222@gmial.com>
2017-12-17 16:33:30 +0100

    json

---
+++ JavaScript/JS/12-petle.js
@@ -0,0 +1,16 @@
+'use strict';
+
+for (var i=4 ; i>0 ; i-- ){
+    console.log(i); 
+}
+
+var tablica =["Krystian", "Monika", "Danuta"] ;
+ tablica.foreach (function(element, index) {
+                  console.log("Element z indexem" + index + "ma wartosc " + element) ;}
+                  )
+for var i=0; i<tablica.lengh; i++ {
+    console.log("Element z indexem" + i + "ma wartosc " + ) ;}
+                  )
+}
+
+
---
+++ JavaScript/JS/13-json.js
@@ -0,0 +1,41 @@
+'use strict';
+var jsonOsoby = {
+     "osoby": [
+         {
+             imie: "Krystian",
+             nazwisko: "Dziopa",
+             wzrost: 180,
+             oczy: "niebieskie",
+             zainteresowania: [
+                 { nazwa: "podroze" },
+                 { nazwa: "gotowanie" }
+             ]
+         },
+         {
+             imie: "Dominik",
+             nazwisko: "Gomuła",
+             wzrost: 170,
+             oczy: "brązowe",
+             zainteresowania: [
+                 { nazwa: "gotowanie" },
+                 { nazwa: "języki" }
+             ]
+         },
+         {
+             imie: "Claudia",
+             nazwisko: "Żelazowska",
+             wzrost: 167,
+             oczy: "brązowe",
+             zainteresowania: [
+                 { nazwa: "programowanie" },
+                 { nazwa: "podroze" },
+                 { nazwa: "języki" }
+             ]
+
+         }
+     ]
+}
+jsonOsoby.osoby.foreach(function(e)
+                        {
+    console.log(imie);
+}) ;
