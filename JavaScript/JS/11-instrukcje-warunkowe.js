f013af5047934c239d3490714c0896901a5cfee0
Damian Pawelec <damianpawelec222@gmial.com>
2017-12-17 15:21:18 +0100

    instrukcjewarunkowe

---
+++ JavaScript/JS/11-instrukcje-warunkowe.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var wzrostMateusza = 100;
+var wzrostOlgi =100; 
+
+if (wzrostOlgi < wzrostMateusza) {
+    console.log("Olga jest wyzsza:)");
+}
+else (wzrostOlgi < wzrostMateusza) {
+    console.log("Olga jest nizsza:)");
+}
