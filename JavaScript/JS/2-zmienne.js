49017f49a21e5a61fc12e6aaeda5b00b8187a53a
Damian Pawelec <damianpawelec222@gmial.com>
2017-12-17 09:53:03 +0100

    zmienne javascript

---
+++ JavaScript/JS/2-zmienne.js
@@ -0,0 +1,13 @@
+'use strict';
+var imie;
+var imieMeskie = "Damian";
+var wiek = 27;
+
+var kolor = "blue";
+
+imie ="Sergio";
+imie = imieMeskie;
+
+console.log(imie);
+console.log(kolor);
+console.log(wiek);
