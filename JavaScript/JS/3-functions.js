a10ea5584334b90d6c2cb829a25a08b81fa9dbba
Damian Pawelec <damianpawelec222@gmial.com>
2017-12-17 11:02:11 +0100

    zmienne

--- JavaScript/JS/3-functions.js
+++ JavaScript/JS/3-functions.js
@@ -1,1 +1,14 @@
 'use strict';
+function obliczOdsetki(kapital, procent) {
+    var kapitalZOdsetkami = kapital + kapital * procent;
+    
+    return kapitalZOdsetkami;
+}
+
+
+var inwestycjaAnety = obliczOdsetki(1000000, 0.015);
+
+console.log( inwestycjaAnety );
+
+var inwestycjaKuby = obliczOdsetki(1000, 0.051);
+console.log(inwestycjaKuby);
