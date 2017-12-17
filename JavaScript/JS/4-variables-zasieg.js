7e6e01f092849d425ad5ac002135df0345aa6177
Damian Pawelec <damianpawelec222@gmial.com>
2017-12-17 11:42:39 +0100

    4 zasieg zmienych

--- JavaScript/JS/4-variables-zasieg.js
+++ JavaScript/JS/4-variables-zasieg.js
@@ -1,1 +1,19 @@
 'use strict'
+
+var ilosc1 = 3500;
+var ilosc2 = 300;
+var ilosc3 = 7500;
+var ilosc4 = 2500;
+
+ilosc2 = 3000; 
+
+function policzSumekasy(iloscOsoby1, iloscOsoby2, iloscOsoby3, iloscosoby4, wiek) {
+    var sumaKasy; 
+    sumaKasy = iloscOsoby1 + iloscOsoby2 + iloscOsoby3 + iloscosoby4; 
+    var osoba =(sumaKasy, wiek); 
+    return osoba; 
+}
+
+var osobaZobliczeniami = policzSumekasy( ilosc1, ilosc2, ilosc3, ilosc4, 25); 
+
+console.log (osobaZobliczeniami); 
